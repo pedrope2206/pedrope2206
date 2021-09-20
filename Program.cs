0ce8beedb4b9c347f1a1d93f23c7812be385cc8f
pedrope2206 <pivettapedro93@gmail.com>
2021-09-20 15:23:13 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ArrayColection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sintaxe do Array : Tiposdedados [] = new tiposdedados [tamanho];
+
+            string[] alunosAMS = new string [5]{
+          
+            "Daniela",
+            "Ana Julia",
+            "Bruno",
+            "Oberdan",
+            "Sofia"
+
+            };  
+
+            Console.WriteLine(alunosAMS[2]);
+
+            //alunosAMS[2] = "Pedro";
+
+            Console.WriteLine($"Novo valor no index: {alunosAMS[2]}");
+
+            foreach ( string alunos in alunosAMS)
+            {
+              Console.WriteLine(alunos);  
+            }
+
+            for (int i = 0; i < alunosAMS.Length; i++ )
+            {
+              Console.WriteLine(alunosAMS[i]);  
+            } 
+
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# comandos do git
+1- git init - (comando para inicializar o git)
+2- git remote add origin https://github.com/pedrope2206/pedrope2206.git 
+3- git remote -v (comando para visualizar o repositório atual)
+4- git commit -m "nome do commit" (faz as alterações em cada commit)
+5- git config --global user.email "pivettapedro93@gmail.com"
+6- git config --global user.name "pedrope2206"
+7-git  git
